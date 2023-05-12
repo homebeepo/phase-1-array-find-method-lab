f00799a8e6c585a6025a9f5d748d2a13067ff7ea
caseyclinch <clinchcasey@gmail.com>
2023-05-12 16:59:43 -0700

    Completed project

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  
+  function didWin(anArray) {
+    return anArray.result === "W"
+  }
+  function superbowlWin(someArray) {
+    if ( someArray.find(didWin) ) {
+        return someArray.find(didWin).year }
+  }
